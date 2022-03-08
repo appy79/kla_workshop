a1a0872712ec8edfaf4e12a3e833f5ec8cc2e372
appy79 <amandeepyadav79@gmail.com>
2022-03-08 14:12:53 +0530

    done MileStone1A

--- parsing/__init__.py
+++ parsing/__init__.py
@@ -5,5 +5,6 @@ logger = logging.getLogger(__name__)
 handler = logging.FileHandler("parsing.log")
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter("%(asctime)s;%(message)s")
+formatter.default_msec_format = "%s.%06d"
 handler.setFormatter(formatter)
 logger.addHandler(handler)
--- parsing/flow.py
+++ parsing/flow.py
@@ -1,4 +1,4 @@
-import time, asyncio
+import time, threading
 from parsing.get_config import current_config
 from parsing import logger
 
@@ -8,12 +8,12 @@ config = current_config()
 config = config[0]
 
 
-def TimeFunction(input, exec_time):
+def TimeFunction(exec_time):
     exec_time = int(exec_time)
     time.sleep(exec_time)
 
 
-def execute_activity(activity, level, exec_type):
+def execute_activity(activity, level):
     if activity["Type"] == "Task":
         if activity["Function"] == "TimeFunction":
             func_input = activity["Inputs"]["FunctionInput"]
@@ -27,25 +27,35 @@ def execute_activity(activity, level, exec_type):
                 + exec_time
                 + ")"
             )
-            if exec_type == "Sequential":
-                TimeFunction(func_input, exec_time)
-            elif exec_type == "Concurrent":
-                asyncio.run(TimeFunction(func_input, exec_time))
+            TimeFunction(exec_time)
     elif activity["Type"] == "Flow":
         execute_workflow(activity, level)
 
 
 def execute_workflow(workflow, level):
-    logger.info(level + " Entry")
     op_type = workflow["Type"]
     exec_type = workflow["Execution"]
+    concurrent = []
     for activity_name, activity in workflow["Activities"].items():
         sub_level = level + "." + activity_name
         logger.info(sub_level + " Entry")
-        execute_activity(activity, sub_level, exec_type)
+        if exec_type == "Sequential":
+            execute_activity(activity, sub_level)
+            logger.info(sub_level + " Exit")
+        elif exec_type == "Concurrent":
+            thread = threading.Thread(
+                target=execute_activity, args=(activity, sub_level)
+            )
+            thread.start()
+            concurrent.append([thread, sub_level])
+
+    for thread, sub_level in concurrent:
+        thread.join()
         logger.info(sub_level + " Exit")
 
 
 for workflow_name, workflow in config.items():
     level = workflow_name
+    logger.info(level + " Exit")
     execute_workflow(workflow, level)
+    logger.info(level + " Exit")
