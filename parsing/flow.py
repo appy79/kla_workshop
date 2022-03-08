d1250a78085fafd3ff070be7bea3b54b5e5cf5d4
appy79 <amandeepyadav79@gmail.com>
2022-03-08 14:17:57 +0530

    Milestone1B done

--- parsing/flow.py
+++ parsing/flow.py
@@ -30,6 +30,7 @@ def execute_activity(activity, level):
             TimeFunction(exec_time)
     elif activity["Type"] == "Flow":
         execute_workflow(activity, level)
+    logger.info(level + " Exit")
 
 
 def execute_workflow(workflow, level):
@@ -41,7 +42,6 @@ def execute_workflow(workflow, level):
         logger.info(sub_level + " Entry")
         if exec_type == "Sequential":
             execute_activity(activity, sub_level)
-            logger.info(sub_level + " Exit")
         elif exec_type == "Concurrent":
             thread = threading.Thread(
                 target=execute_activity, args=(activity, sub_level)
@@ -51,11 +51,10 @@ def execute_workflow(workflow, level):
 
     for thread, sub_level in concurrent:
         thread.join()
-        logger.info(sub_level + " Exit")
 
 
 for workflow_name, workflow in config.items():
     level = workflow_name
-    logger.info(level + " Exit")
+    logger.info(level + " Entry")
     execute_workflow(workflow, level)
     logger.info(level + " Exit")
